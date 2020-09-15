2e93ee3960e1da3ad15e07087c6ce9ef3f4184fd
Yahjairav23 <vyahjaira23@gmail.com>
2020-09-14 21:21:03 -0400

    learning while loops

---
+++ Loops.java
@@ -0,0 +1,12 @@
+
+public class Loops {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println("I am " + i);
+            i++;
+        }
+        
+    }
+}
