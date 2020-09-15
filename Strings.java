d1ed3fb2c5b13229ba67cba8a0952da21822dd41
Yahjairav23 <vyahjaira23@gmail.com>
2020-09-14 21:13:51 -0400

    learning the String object

---
+++ Strings.java
@@ -0,0 +1,13 @@
+
+public class Strings {
+    public static void main(String[] args) {
+        String text = "Hello!";
+        // String is an object
+        String opener = "My name is Yahjaira.";
+
+        String ageIntro = "How old am I?";
+        int age = 27;
+
+        System.out.println(text + " " + opener + " " + ageIntro + " " + age);
+    }
+}
