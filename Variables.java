e5b071c1fc57390c2c503591c8af9f17bcd8fd72
Yahjairav23 <vyahjaira23@gmail.com>
2020-09-14 21:02:41 -0400

    learning variable types

---
+++ Variables.java
@@ -0,0 +1,28 @@
+
+public class Variables {
+    public static void main(String[] args) {
+        int aValue;
+        aValue = 10;
+
+        short aShort = 40;
+        long aLong = 10293;
+
+        double aDouble = 2.462;
+        float aFloat = 1.4f;
+
+        char aChar = 'b';
+
+        boolean aBoolean = true;
+
+        byte aByte = 127;
+
+        System.out.println(aValue);
+        System.out.println(aShort);
+        System.out.println(aLong);
+        System.out.println(aDouble);
+        System.out.println(aFloat);
+        System.out.println(aChar);
+        System.out.println(aBoolean);
+        System.out.println(aByte);
+    }
+}
