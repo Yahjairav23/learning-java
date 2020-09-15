bc2265df599500aa1ddb59ee54350bf740710106
Yahjairav23 <vyahjaira23@gmail.com>
2020-09-14 21:25:23 -0400

    learning for loops

--- Loops.java
+++ Loops.java
@@ -4,9 +4,13 @@ public class Loops {
 
         int i = 0;
         while (i < 5) {
-            System.out.println("I am " + i);
+            System.out.println("While I am " + i);
             i++;
         }
-        
+
+        for(int j = 5; j > 0; j--){
+            System.out.println("For I am " + j);
+        }
+
     }
 }
