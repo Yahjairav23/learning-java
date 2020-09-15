451889368d9b82a989b20e4018480943bc0289f4
Yahjairav23 <vyahjaira23@gmail.com>
2020-09-14 21:28:55 -0400

    learning if statements

--- Loops.java
+++ Loops.java
@@ -1,16 +1,18 @@
 
 public class Loops {
     public static void main(String[] args) {
-
-        int i = 0;
-        while (i < 5) {
-            System.out.println("While I am " + i);
-            i++;
+        int x = 2;
+        
+        if (x == 5) {
+            int i = 0;
+            while (i < 5) {
+                System.out.println("While I am " + i);
+                i++;
+            }
+        } else {
+            for(int j = 5; j > 0; j--){
+                System.out.println("For I am " + j);
+            }
         }
-
-        for(int j = 5; j > 0; j--){
-            System.out.println("For I am " + j);
-        }
-
-    }
+}
 }
