4755456099052d632bedc33e018d22e9a556ef60
Yahjairav23 <vyahjaira23@gmail.com>
2020-09-17 14:39:36 -0400

    do while conditionals

---
+++ DoWhile.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+        
+        do {
+            System.out.println("Guess a number, any number! ");
+        number = input.nextInt();
+
+        }
+        while(number != 7);
+        if (number == 7) {
+            System.out.println("Congrats! You guessed right!");
+        }
+
+    }
+}
